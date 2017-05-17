a96ccf3c93232ec2913bb441983b83536f8e3f81
Yolanda M. Garcia <yolanda.garcia16@tectijuana.edu.mx>
2017-05-17 11:28:48 -0700

    primer commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'welcome#home'
+
+resources :articles
+get 'signup', to: 'users#new'
+resources :users, except: [:new]
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
+
+end
